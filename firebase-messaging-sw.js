ff6f2f913cd49c1678cf7dec3e1260c057702283
yanwhite1017 <52163207+yanwhite1017@users.noreply.github.com>
2020-11-17 17:58:57 +0500

    Add files via upload

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,22 @@
+ importScripts('https://www.gstatic.com/firebasejs/7.20.0/firebase-app.js');
+ importScripts('https://www.gstatic.com/firebasejs/7.20.0/firebase-messaging.js');
+ // Initialize the Firebase app in the service worker by passing in
+ // your app's Firebase config object.
+ // https://firebase.google.com/docs/web/setup#config-object
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCd8HZXs3XZeRKxW5iKTPt_IqSQFL0A4wo",
+  authDomain: "push-app-66813.firebaseapp.com",
+  databaseURL: "https://push-app-66813.firebaseio.com",
+  projectId: "push-app-66813",
+  storageBucket: "push-app-66813.appspot.com",
+  messagingSenderId: "702874769745",
+  appId: "1:702874769745:web:b3a5e69d1aadd6773d1b70",
+  measurementId: "G-Z66CVTGBK4"
+};
+ // Retrieve an instance of Firebase Messaging so that it can handle background
+ // messages.
+firebase.messaging.isSupported();
+ const messaging = firebase.messaging();
+ // [END initialize_firebase_in_sw]
+ **/
