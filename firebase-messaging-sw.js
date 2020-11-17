9a7e68946ba76e0a9c88e461ed533a2d8b8892b7
yanwhite1017 <52163207+yanwhite1017@users.noreply.github.com>
2020-11-17 18:01:04 +0500

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,5 @@
- importScripts('https://www.gstatic.com/firebasejs/7.20.0/firebase-app.js');
- importScripts('https://www.gstatic.com/firebasejs/7.20.0/firebase-messaging.js');
+ importScripts('https://www.gstatic.com/firebasejs/8.0.2/firebase-app.js');
+ importScripts('https://www.gstatic.com/firebasejs/8.0.2/firebase-messaging.js');
  // Initialize the Firebase app in the service worker by passing in
  // your app's Firebase config object.
  // https://firebase.google.com/docs/web/setup#config-object
@@ -16,7 +16,5 @@ const firebaseConfig = {
 };
  // Retrieve an instance of Firebase Messaging so that it can handle background
  // messages.
-firebase.messaging.isSupported();
  const messaging = firebase.messaging();
  // [END initialize_firebase_in_sw]
- **/
